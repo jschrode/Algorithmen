365ffe0986bba6166dba08f066dde41cf0ed28d5
jschrode <schrode@hm.edu>
2018-04-26 09:50:30 +0200

    Aufgabenblatt 6

---
+++ QuickSorter.java
@@ -0,0 +1,34 @@
+package hm.edu.cs.algdat.sorting.sorters;
+
+import hm.edu.cs.algdat.sorting.framework.AbstractSorter;
+import hm.edu.cs.algdat.sorting.framework.Sortable;
+
+/**
+ * Implementierung von Quick.Sort als {@link AbstractSorter}.
+ * 
+ * @author katz.bastian
+ */
+public class QuickSorter extends AbstractSorter {
+
+	@Override
+	protected <T extends Comparable<T>> void sort(Sortable<T> input) {
+		sort(input, 0, input.length() - 1);
+	}
+
+	private <T extends Comparable<T>> void sort(Sortable<T> input, int lo, int hi) {
+		if (hi <= lo)
+			return;
+		int pivotIndex = (4 * lo + hi) / 5;
+		int j = partition(input, lo, hi, pivotIndex);
+		sort(input, lo, j - 1);
+		sort(input, j + 1, hi);
+	}
+
+	private <T extends Comparable<T>> int partition(Sortable<T> input, int lo, int hi, int pivotIndex) {
+		// TODO: Implementierung der Pivotisierung. Zurückgegeben wird die
+		// Position des Pivotelementes nach der Pivotisierung
+
+		return 0;
+	}
+
+}
