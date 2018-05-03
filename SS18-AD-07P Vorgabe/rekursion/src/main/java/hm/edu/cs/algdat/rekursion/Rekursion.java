90f7a20fc95ff4d90aadcab1f2c3272784ed40af
jschrode <schrode@hm.edu>
2018-05-03 11:32:23 +0200

    Aufgabenblatt 7 teilweise

--- SS18-AD-07P Vorgabe/rekursion/src/main/java/hm/edu/cs/algdat/rekursion/Rekursion.java
+++ SS18-AD-07P Vorgabe/rekursion/src/main/java/hm/edu/cs/algdat/rekursion/Rekursion.java
@@ -7,9 +7,14 @@ public class Rekursion {
 	 * Variante.
 	 * 
 	 * @param string
-	 * @return
+	 * @return true if Palindrome, false else
 	 */
 	static boolean isPalindromeIterative(String string) {
+		String temp = string.toUpperCase();
+		if (temp.length() == 0) return true;
+		for (int i = 0; i <= (temp.length() / 2); i++) {
+			if (temp.charAt(i) != temp.charAt(temp.length() - 1 - i)) return false;
+		}
 		return true;
 	}
 
@@ -18,11 +23,16 @@ public class Rekursion {
 	 * Variante.
 	 * 
 	 * @param string
-	 * @return
+	 * @return true if Palindrome, false else
 	 */
 	static boolean isPalindromeRecursive(String string) {
-		return true;
+		return isPalindromeRecursive(string, 0, string.length() - 1);
 	}
 
-	
+	private static boolean isPalindromeRecursive(String string, int von, int bis) {
+		String temp = string.toUpperCase();
+		if (von >= bis) return true;
+		if (temp.charAt(von) == temp.charAt(bis)) return isPalindromeRecursive(temp, von + 1, bis - 1);
+		return false;
+	}
 }
